9df9b10ea6b3a8b114b7b90b463a6c91a4bde269
cteryue <2016201028@qq.com>
2023-03-16 13:22:59 +0800

    jwt测试 2

--- src/main/java/com/chentian/ssjwt/service/impl/AccountServiceImpl.java
+++ src/main/java/com/chentian/ssjwt/service/impl/AccountServiceImpl.java
@@ -18,7 +18,7 @@ public class AccountServiceImpl implements AccountService {
     public Account findByUsername(String username) {
         QueryWrapper wrapper = new QueryWrapper();
         wrapper.eq("username",username);
-        System.out.println(1);
+        System.out.println(2);
         return accountMapper.selectOne(wrapper);
     }
 }
